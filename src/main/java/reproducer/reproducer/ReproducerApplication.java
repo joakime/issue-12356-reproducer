2f4dd8d9218d2ba267644be95ec4ecb4b20a4a8b
guenther.gsenger <guenther.gsenger@dynatrace.com>
2024-10-10 11:48:01 +0200

    Add controller, use Jetty

--- src/main/java/reproducer/reproducer/ReproducerApplication.java
+++ src/main/java/reproducer/reproducer/ReproducerApplication.java
@@ -1,16 +1,15 @@
 package reproducer.reproducer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootConfiguration
+@SpringBootApplication(scanBasePackages = "reproducer.reproducer")
 @EnableAutoConfiguration
 public class ReproducerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReproducerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReproducerApplication.class, args);
+    }
 
 }
